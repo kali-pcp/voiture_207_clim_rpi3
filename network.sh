6c160d5fa5a0d0193450e1b767030d0bc5705d96
kali-pcp <tony.bernardini2b@gmail.com>
2022-02-26 13:19:30 +0100

    Suppresion de fichier inutile

--- network.sh
+++ network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "ttfhbfnk">ss.txt
-#sudo systemctl start networking.service
-#sudo systemctl start wpa_supplicant.service 
-#sudo systemctl start ssh.service 
-#sudo systemctl start dhcpcd
+
+sudo systemctl start networking.service
+sudo systemctl start wpa_supplicant.service 
+sudo systemctl start ssh.service 
+sudo systemctl start dhcpcd
