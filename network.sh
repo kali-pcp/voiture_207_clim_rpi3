8f59a2dd276fa1d1eb8bdf2c2253abd16f1763b6
Tony <tony.bernardini2b@gmail.com>
2022-08-10 14:18:48 +0200

    Page web

--- network.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo systemctl restart deamon-reload
-sudo systemctl start networking.service
-sudo systemctl start wpa_supplicant.service 
-sudo systemctl start ssh.service 
-sudo systemctl start dhcpcd
