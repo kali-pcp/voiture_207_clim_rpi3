3e7c71b8ab05b487bfbd7f76be64a872454a7f34
Tony <tony.bernardini2b@gmail.com>
2022-08-10 14:15:53 +0200

    Page web

--- Can/Changer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-while read p; do
-  echo "$p"
-  echo $(echo ${test} | cut -d' ' -f3) ### sort 1D0#22000000000909
-done <peptides.txt
--- Can/Setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo ip link set can0 up type can bitrate 125000 sample-point 0.875 
